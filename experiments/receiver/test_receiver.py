42f27146165ba69330933af480de2f3cbe64918e
Yilong Geng <gengyl08@DNab4046a3.stanford.edu>
2015-01-14 16:17:46 -0800

    bug fix

--- experiments/receiver/test_receiver.py
+++ experiments/receiver/test_receiver.py
@@ -1,57 +1,54 @@
+#!/usr/bin/env python
+
 import subprocess
 import argparse
 
-interrupts_keys = ['interrupts']
-ifconfig_keys = ['rx_packets', 'rx_segments', 'rx_errors', 'rx_dropped',
-                 'tx_packets', 'tx_segments', 'tx_errors', 'tx_dropped']
-prune_keys = ['pruned_from_rx_q_overrun', 'pruned_from_rx_q', 'dropped_from_out_of_order_q_overrun',
-              'pkts_dropped_from_prequeue']
-ack_keys = ['delayed_acks', 'further_delayed_acks', 'quick_ack_mode_activated']
-receive_queue_keys = ['pkts_directly_queued_to_recvmsg_prequeue', 'pkts_received_from_backlog',
-                      'pkts_received_from_prequeue']
-header_prediction_keys = ['pkts_header_predicted', 'pkts_header_predicted_and_directly_queued_to_user']
+interrupt_keys = ['eth0-rx', 'eth0-tx']
+
+ifconfig_keys = ['rx_packets', 'rx_errors', 'rx_dropped', 'rx_bytes',
+                 'tx_packets', 'tx_errors', 'tx_dropped', 'tx_bytes']
+
+segment_keys = {'segments received', 'segments send out', 'segments retransmited', 'bad segments received'}
+
+tcpExt_keys = subprocess.Popen('cat /proc/net/netstat'.split(), stdout=subprocess.PIPE).communicate()[0].split('\n')[0].split()[1:]
 
 parser = argparse.ArgumentParser(description='Argument Parser')
 parser.add_argument('-c', '--core', nargs=1, default=0, help='the core iperf runs on', type=int)
-parser.add_argument('--offload', nargs=1, help='turn on/off large receive offload', action='store_true')
+parser.add_argument('--offload_off', help='turn on/off various TCP offloads', action='store_true')
 
 def read_stats():
-    netstat = subprocess.check_output('netstat -s'.split())
-    ifconfig = subprocess.check_output('ifconfig eth1'.split())
-    interrupts = subprocess.check_output('grep eth1 /proc/interrupts'.split())
+    ifconfig_str = subprocess.Popen('ifconfig eth0'.split(), stdout=subprocess.PIPE).communicate()[0]
+    netstat_str = subprocess.Popen('netstat -s'.split(), stdout=subprocess.PIPE).communicate()[0]
+    tcpExt_str = subprocess.Popen('cat /proc/net/netstat'.split(), stdout=subprocess.PIPE).communicate()[0]
 
     result = {}
-    # ifconfig
-    lines = ifconfig.split('\n')
-    result['rx_packets'] = int(lines[3].split()[1].split(':')[1])
-    result['rx_errors'] = int(lines[3].split()[2].split(':')[1])
-    result['rx_dropped'] = int(lines[3].split()[3].split(':')[1])
-    result['tx_packets'] = int(lines[4].split()[1].split(':')[1])
-    result['tx_errors'] = int(lines[4].split()[2].split(':')[1])
-    result['tx_dropped'] = int(lines[4].split()[3].split(':')[1])
 
-    # interrupts
-    result['interrupts'] = int(interrupts.split()[1])
+    # interrupt
+    result['eth0-rx'] = int(subprocess.Popen('grep eth0-rx /proc/interrupts'.split(), stdout=subprocess.PIPE).communicate()[0].split()[1])
+    result['eth0-tx'] = int(subprocess.Popen('grep eth0-tx /proc/interrupts'.split(), stdout=subprocess.PIPE).communicate()[0].split()[1])
+
+    # ifconfig
+    lines = ifconfig_str.split('\n')
+    result['rx_packets'] = int(lines[4].split()[1].split(':')[1])
+    result['rx_errors'] = int(lines[4].split()[2].split(':')[1])
+    result['rx_dropped'] = int(lines[4].split()[3].split(':')[1])
+    result['tx_packets'] = int(lines[5].split()[1].split(':')[1])
+    result['tx_errors'] = int(lines[5].split()[2].split(':')[1])
+    result['tx_dropped'] = int(lines[5].split()[3].split(':')[1])
+    result['rx_bytes'] = int(lines[7].split()[1].split(':')[1])
+    result['tx_bytes'] = int(lines[7].split()[5].split(':')[1])
+
+    # tcp segments
+    for key in segment_keys:
+        for line in netstat_str.split('\n'):
+            if line.find(key) >= 0:
+                result[key] = int(line.split()[0])
+                break;
 
     # netstat
-    lines = netstat.split('\n')
-    for i in range(len(lines)):
-        print i, lines[i]
-    result['rx_segments'] = int(lines[35].split()[0])
-    result['tx_segments'] = int(lines[36].split()[0])
-    result['rx_segments_bad'] = int(lines[38].split()[0])
-    result['pruned_from_rx_q_overrun'] = int(lines[49].split()[0])
-    result['pruned_from_rx_q'] = int(lines[50].split()[0])
-    result['dropped_from_out_of_order_q_overrun'] = int(lines[51].split()[0])
-    result['delayed_acks'] = int(lines[55].split()[0])
-    result['further_delayed_acks'] = int(lines[56].split()[0])
-    result['quick_ack_mode_activated'] = int(lines[57].split()[5])
-    result['pkts_directly_queued_to_recvmsg_prequeue'] = int(lines[60].split()[0])
-    result['pkts_received_from_backlog'] = int(lines[61].split()[0])
-    result['pkts_received_from_prequeue'] = int(lines[62].split()[0])
-    result['pkts_dropped_from_prequeue'] = int(lines[63].split()[0])
-    result['pkts_header_predicted'] = int(lines[64].split()[0])
-    result['pkts_header_predicted_and_directly_queued_to_user'] = int(lines[65].split()[0])
+    tcpExt_values = [int(x) for x in tcpExt_str.split('\n')[1].split()[1:]]
+    for i in range(len(tcpExt_keys)):
+        result[tcpExt_keys[i]] = tcpExt_values[i]
 
     return result
 
@@ -60,15 +57,20 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    if args.lro_off:
-        subprocess.call('sudo ethtool -K eth1 lro off'.split())
-        subprocess.call('sudo ethtool -K eth1 gro off'.split())
+    if not args.offload_off:
+        print 'tso, gso and gro on'
+        subprocess.call('sudo ethtool -K eth0 tso on'.split())
+        subprocess.call('sudo ethtool -K eth0 gso on'.split())
+        subprocess.call('sudo ethtool -K eth0 gro on'.split())
     else:
-        subprocess.call('sudo ethtool -K eth1 lro on'.split())
-        subprocess.call('sudo ethtool -K eth1 gro on'.split())
+        print 'tso, gso and gro off'
+        subprocess.call('sudo ethtool -K eth0 tso off'.split())
+        subprocess.call('sudo ethtool -K eth0 gso off'.split())
+        subprocess.call('sudo ethtool -K eth0 gro off'.split())
 
-    #result1 = read_stats()
+    result1 = read_stats()
 
+    print 'running iperf on core %d' % args.core
     p = subprocess.Popen(['taskset', '-c', str(args.core), 'iperf', '-s'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in iter(p.stdout.readline, b''):
         print line
@@ -76,34 +78,23 @@ if __name__=="__main__":
             break
     p.kill()
 
-    #result2 = read_stats()
-    """
+    result2 = read_stats()
+    
     print '========== Interrupts =========='
-    for key in interrupts_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
+    for key in interrupt_keys:
+        print result2[key] - result1[key], key
 
     print ''
     print '========== Ifconfig =========='
     for key in ifconfig_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
+        print result2[key] - result1[key], key
+    
+    print ''
+    print '========== TCP =========='
+    for key in segment_keys:
+        print result2[key] - result1[key], key
 
     print ''
-    print '========== ACKs =========='
-    for key in ack_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
-
-    print ''
-    print '========== Drops =========='
-    for key in prune_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
-
-    print ''
-    print '========== Receive Queues =========='
-    for key in receive_queue_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
-
-    print ''
-    print '========== Header Prediction =========='
-    for key in header_prediction_keys:
-        print result2[key] - result1[key], ' '.join(key.split('_'))
-    """
+    print '========== TcpExt =========='
+    for key in tcpExt_keys:
+        print result2[key] - result1[key], key
