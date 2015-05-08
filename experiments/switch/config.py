dd75c5af399b60362a5d22f3c31232fd5812c9a3
Yilong Geng <gengyl08@stanford.edu>
2015-05-07 18:11:17 -0700

    update config.py according to new sram hardware

--- experiments/switch/config.py
+++ experiments/switch/config.py
@@ -40,28 +40,18 @@ import argparse
 
 DATAPATH_FREQUENCY = 160000000
 
-REORDER_OUTPUT_QUEUES_BASE_ADDR = "0x74400000"
+REORDER_OUTPUT_QUEUES_BASE_ADDR = "0x76000000"
 
-DELAY_BASE_ADDR = {0 : "0x79c80000",
-                   1 : "0x79c60000",
-                   2 : "0x79c40000",
-                   3 : "0x79c20000",
-                   4 : "0x79c00000"}
-
-"""
-RATE_LIMITER_BASE_ADDR = {0 : "0x77e80000",
-                          1 : "0x77e60000",
-                          2 : "0x77e40000",
-                          3 : "0x77e20000",
-                          4 : "0x77e00000"}
-"""
+DELAY_BASE_ADDR = {0 : "0x79c60000",
+                   1 : "0x79c40000",
+                   2 : "0x79c20000",
+                   3 : "0x79c00000"}
 
 parser = argparse.ArgumentParser(description='Argument Parser')
-parser.add_argument('--delay', help='delay values for each queue', nargs=5, default=[0, 0, 0, 0, 0], type=int)
-parser.add_argument('--dropLoop', help='drop_loop for each queue', nargs=5, default=[0, 0, 0, 0, 0], type=int)
-parser.add_argument('--dropCount', help='drop_count for each queue', nargs=5, default=[0, 0, 0, 0, 0], type=int)
-parser.add_argument('--splitRatio', help='split_ratio for each queue', nargs=5, default=[1, 1, 1, 1, 1], type=int)
-parser.add_argument('--resetDrop', help='reset drop counters', action='store_true')
+parser.add_argument('--delay', help='delay values for each queue', nargs=4, default=[0, 0, 0, 0], type=int)
+parser.add_argument('--dropLoop', help='drop_loop for each queue', nargs=4, default=[0, 0, 0, 0], type=int)
+parser.add_argument('--dropCount', help='drop_count for each queue', nargs=4, default=[0, 0, 0, 0], type=int)
+parser.add_argument('--splitRatio', help='split_ratio for each queue', nargs=4, default=[1, 0, 0, 0], type=int)
 parser.add_argument('--printDrop', help='print drop counters', action='store_true')
 parser.add_argument('--printDelay', help='print delays', action='store_true')
 parser.add_argument('--printSplitRatio', help='print split ratios', action='store_true')
@@ -70,72 +60,47 @@ class ReorderOutputQueues:
 
     def __init__(self):
         self.module_base_addr = REORDER_OUTPUT_QUEUES_BASE_ADDR
-        self.reset_drop_counts_reg_offset = "0x01"
-        self.drop_counts_reg_offset = ["0x10", "0x11", "0x12", "0x13", "0x14"]
-        self.split_ratios_reg_offset = ["0x20", "0x21", "0x22", "0x23"]
+        self.drop_counts_reg_offset = ["0x3", "0x4", "0x5", "0x6"]
+        self.split_ratios_reg_offset = ["0x0", "0x1", "0x2"]
 
-        self.reset_drop_counts = False
-        self.drop_counts = [0, 0, 0, 0, 0]
-        self.split_ratios = [0, 0, 0, 0, 0]
+        self.drop_counts = [0, 0, 0, 0]
+        self.split_ratios = [0, 0, 0, 0]
 
-        self.get_reset_drop_counts()
         self.get_drop_counts()
         self.get_split_ratios()
 
-    def get_reset_drop_counts(self):
-        value = rdaxi(self.reg_addr(self.reset_drop_counts_reg_offset))
-        value = int(value, 16)
-        if value == 0:
-            self.reset_drop_counts = False;
-        else:
-            self.reset_drop_counts = True;
-
-    def set_reset_drop_counts(self, reset):
-        if reset:
-            value = 1
-        else:
-            value = 0
-        wraxi(self.reg_addr(self.reset_drop_counts_reg_offset), hex(value))
-        self.get_reset_drop_counts()
-        self.get_drop_counts()
-
     def get_drop_counts(self):
-        for i in range(5):
+        for i in range(4):
             drop_count = rdaxi(self.reg_addr(self.drop_counts_reg_offset[i]))
             self.drop_counts[i] = int(drop_count, 16)
 
     def get_split_ratios(self):
-        for i in range(4):
+        for i in range(3):
             split_ratio = rdaxi(self.reg_addr(self.split_ratios_reg_offset[i]))
             self.split_ratios[i] = float(int(split_ratio, 16))
 
         self.split_ratios = [x/4294967295 for x in self.split_ratios]
-        self.split_ratios[4] = 1.0
+        self.split_ratios[3] = 1.0
 
-        for i in range(1, 5):
+        for i in range(1, 3):
             self.split_ratios[i] = max(0, self.split_ratios[i] - sum(self.split_ratios[:i]))
 
     def set_split_ratios(self, split_ratios):
         tmp = sum(split_ratios)
         split_ratios = [float(x)/tmp for x in split_ratios]
 
-        for i in range(1, 5):
+        for i in range(1, 4):
             split_ratios[i] = split_ratios[i-1] + split_ratios[i]
 
-        for i in range(4):
+        for i in range(3):
             split_ratios[i] = int(split_ratios[i] * 4294967295)
 
-        for i in range(4):
+        for i in range(3):
             wraxi(self.reg_addr(self.split_ratios_reg_offset[i]), hex(split_ratios[i]))
         self.get_split_ratios()
 
     def reg_addr(self, offset):
         return add_hex(self.module_base_addr, offset)
-
-    def print_status(self):
-        print "Reset Drop Counts: " + str(self.reset_drop_counts)
-        for i in range(5):
-            print "Queue " + str(i) + " Drop Count: " + str(self.drop_counts[i])
 
     def print_drop(self):
         for i in range(5):
@@ -204,29 +169,13 @@ if __name__=="__main__":
     #rateLimiters = {}
     args = parser.parse_args()
 
-    delays = {}
-
     reorderOutputQueues = ReorderOutputQueues()
-    
-    # instantiate rate limiters and delay modules for 4 interfaces
-    for i in range(5):
-        # add rate limiter for that interface
-        #rateLimiters.update({i : RateLimiter(i)})
-        # add delay module for that interface
+
+    delays = {}
+    for i in range(4):
         delays.update({i : Delay(i)})
 
-    """
-    # configure rate limiters
-    for iface, rl in rateLimiters.iteritems():
-        rl.set_rate(0)
-        rl.set_enable(False)
-        rl.print_status()
-    """
-
     reorderOutputQueues.set_split_ratios(args.splitRatio)
-    if args.resetDrop:
-        reorderOutputQueues.set_reset_drop_counts(True)
-        reorderOutputQueues.set_reset_drop_counts(False)
 
     if args.printDrop:
         reorderOutputQueues.print_drop()
