e3c7521096b45a3a5a70e905a8de3eb351702684
Yilong Geng <gengyl08@stanford.edu>
2015-05-20 12:37:43 -0700

    new controller code

--- experiments/controller/run.py
+++ experiments/controller/run.py
@@ -6,11 +6,11 @@ import subprocess
 switch = 'gengyl08@nf3-test6.stanford.edu'
 sender = 'gengyl08@chaos1.stanford.edu'
 receiver = 'gengyl08@chaos2.stanford.edu'
-home = '/home/gengyl08/BalajiGroup/packet_reordering/experiments'
+home = '/home/gengyl08/BalajiGroup/packet_reordering'
 
 def measure(delay, drop_loop, drop_count, is_new_kernel, flow_num):
 	# configure switch
-    subprocess.call('ssh -t {0} sudo python {1}/switch/config.py 1 --delay 0 {2} 0 0 0 --dropLoop {3} {3} 0 0 0 --dropCount {4} {4} 0 0 0 --printQueueNum --printDelay'.format(switch, home, delay, drop_loop, drop_count).split())
+    subprocess.call('ssh -t {0} sudo python {1}/experiments/switch/config.py 1 --delay 0 {2} 0 0 0 --dropLoop {3} {3} 0 0 0 --dropCount {4} {4} 0 0 0 --printQueueNum --printDelay'.format(switch, home, delay, drop_loop, drop_count).split())
 
     # configure old/new kernel on the receiver
     
@@ -55,12 +55,73 @@ def measure(delay, drop_loop, drop_count, is_new_kernel, flow_num):
     
     return result_th, result_cpu_sender, result_cpu0, result_cpu1
 
+def measure_perf(delay, split_ratio, drop_loop, drop_count, is_new_kernel, flow_num):
+    # configure switch
+    print '>>>>>>>>>> Configure Switch'
+    subprocess.call('ssh -t {0} sudo python {1}/experiments/switch/config.py --delay {2[0]} {2[1]} {2[2]} {2[3]} --splitRatio {3[0]} {3[1]} {3[2]} {3[3]} --dropLoop {4[0]} {4[1]} {4[2]} {4[3]} --dropCount {5[0]} {5[1]} {5[2]} {5[3]} --printDelay --printSplitRatio'.format(switch, home, delay, split_ratio, drop_loop, drop_count).split())
+
+    # configure old/new kernel on the receiver
+
+    # get statistics before sending packets
+    print '>>>>>>>>>> Get TX Packets on Receiver'
+    tx_pkts_before = int(subprocess.check_output('ssh {0} ifconfig eth0'.format(receiver).split()).split('\n')[5].split()[1].split(':')[1])
+    print '>>>>>>>>>> Get Segments Received on Receiver'
+    segs_received_before = int(subprocess.check_output('ssh {0} netstat -s | grep -m 1 \'segments received\''.format(receiver).split()).split()[0])
+
+    # open perf server on the receiver
+    print '>>>>>>>>>> Open Perf on Receiver'
+    p_receiver = subprocess.Popen('ssh {0} taskset -c 1 {1}/perf/tcp_stream -H 192.168.0.2 -F {2}'.format(receiver, home, flow_num).split(), stdout=subprocess.PIPE)
+
+    # open flow_num of iperf flows on the sender
+    print '>>>>>>>>>> Open Perf on Sender'
+    p_sender = subprocess.Popen('ssh {0} taskset -c 1 {1}/perf/tcp_stream -c -l 20 -H 192.168.0.2 -F {2}'.format(sender, home, flow_num).split(), stdout=subprocess.PIPE)
+    
+    # measure cpu utilization on the sender receiver
+    print '>>>>>>>>>> Sleep for 5 Seconds'
+    time.sleep(5)
+    print '>>>>>>>>>> Open Mpstat on Receiver'
+    p_cpu_receiver = subprocess.Popen('ssh {0} mpstat -P ALL 10 1'.format(receiver).split(), stdout=subprocess.PIPE)
+    print '>>>>>>>>>> Open Mpstat on Sender'
+    p_cpu_sender = subprocess.Popen('ssh {0} mpstat -P ALL 10 1'.format(sender).split(), stdout=subprocess.PIPE)
+    
+
+    # wait for all threads to complete
+    print '>>>>>>>>>> Wait for ALL Processes to Finish'
+    cpu_util_receiver, err_cpu = p_cpu_receiver.communicate()
+    print '    >>>>>>>>>> Receiver Mpstat Finished'
+    cpu_util_sender, err_cpu = p_cpu_sender.communicate()
+    print '    >>>>>>>>>> Sender Mpstat Finished'
+    p_sender.communicate()
+    print '    >>>>>>>>>> Sender Perf Finished'
+    throughput, err = p_receiver.communicate()
+    print '    >>>>>>>>>> Receiver Perf Finished'
+
+    # get statistics after sending packets
+    print '>>>>>>>>>> Get TX Packets on Receiver'
+    tx_pkts_after = int(subprocess.check_output('ssh {0} ifconfig eth0'.format(receiver).split()).split('\n')[5].split()[1].split(':')[1])
+    print '>>>>>>>>>> Get Segments Received on Receiver'
+    segs_received_after = int(subprocess.check_output('ssh {0} netstat -s | grep -m 1 \'segments received\''.format(receiver).split()).split()[0])
+
+    # compute results
+    result_th = float(throughput.split('\n')[17].split('=')[1])/1000
+    result_cpu_sender = 100 - float(cpu_util_sender.split('\n')[5].split()[-1])
+    result_cpu_receiver = 100 - float(cpu_util_receiver.split('\n')[5].split()[-1])
+    result_segs_received = segs_received_after - segs_received_before
+    result_tx_pkts = tx_pkts_after - tx_pkts_before
+
+    print '>>>>>>>>>> Results:'
+    print '    >>>>>>>>>> Throughput: ' + str(result_th)
+    print '    >>>>>>>>>> Sender CPU: ' + str(result_cpu_sender)
+    print '    >>>>>>>>>> Receiver CPU: ' + str(result_cpu_receiver)
+    print '    >>>>>>>>>> Segments Received by Receiver: ' + str(result_segs_received)
+    print '    >>>>>>>>>> Packets sent by Receiver: ' + str(result_tx_pkts)
+    
+    return result_th, result_cpu_sender, result_cpu_receiver, result_segs_received, result_tx_pkts
+
 if __name__=="__main__":
-	f = open('measurements.txt', 'w')
-	for flow_num in range(1, 5):
-		for drop_loop in [0, 100, 1000, 10000, 100000, 1000000]:
-			for delay in [1600*x for x in [0, 1, 4, 6]]:
-				th, cpu_sender, cpu0, cpu1 = measure(delay, drop_loop, int(drop_loop!=0), True, flow_num)
-				f.write('%d %d %d %f %f %f %f\n' % (flow_num, drop_loop, delay, th, cpu_sender, cpu0, cpu1))
-				print ('%d %d %d %f %f %f %f\n' % (flow_num, drop_loop, delay, th, cpu_sender, cpu0, cpu1))
-	f.close()
+    delay = [0, 0, 0, 0]
+    split_ratio = [1, 0, 0, 0]
+    drop_loop = [0, 0, 0, 0]
+    drop_count = [0, 0, 0, 0]
+    result_th, result_cpu_sender, result_cpu_receiver, result_segs_received, result_tx_pkts = measure_perf(delay, split_ratio, drop_loop, drop_count, True, 1)
+    print result_th, result_cpu_sender, result_cpu_receiver, result_segs_received, result_tx_pkts
