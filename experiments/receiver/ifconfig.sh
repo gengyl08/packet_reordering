4740a232cb67eee5dae6a9ae413f2beb7155067b
Yilong Geng <gengyl08@stanford.edu>
2015-06-11 22:32:49 -0700

    update server config script

--- experiments/receiver/ifconfig.sh
+++ experiments/receiver/ifconfig.sh
@@ -13,9 +13,14 @@ intr3=$(cat /proc/interrupts | grep eth0-rx-3 | grep -oP '[0-9]+' | head -n 1)
 intr4=$(cat /proc/interrupts | grep eth0-tx-0 | grep -oP '[0-9]+' | head -n 1)
 
 echo 000001 > /proc/irq/$intr0/smp_affinity
+echo 0 > /proc/irq/$intr0/smp_affinity_list
 echo 000002 > /proc/irq/$intr1/smp_affinity
+echo 1 > /proc/irq/$intr1/smp_affinity_list
 echo 000004 > /proc/irq/$intr2/smp_affinity
+echo 2 > /proc/irq/$intr2/smp_affinity_list
 echo 000008 > /proc/irq/$intr3/smp_affinity
+echo 3 > /proc/irq/$intr3/smp_affinity_list
 echo 000010 > /proc/irq/$intr4/smp_affinity
+echo 4 > /proc/irq/$intr4/smp_affinity_list
 
 sysctl -w net.ipv4.tcp_congestion_control=reno
