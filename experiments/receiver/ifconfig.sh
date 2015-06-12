41e2042611a838bec69510e139f92d078cfab4f0
Yilong Geng <gengyl08@chaos2.stanford.edu>
2015-06-11 18:00:26 -0700

    update server config script

---
+++ experiments/receiver/ifconfig.sh
@@ -0,0 +1,16 @@
+ifconfig eth0 192.168.0.2 mask 255.255.255.0
+ifconfig eth1 192.168.1.2 mask 255.255.255.0
+ifconfig eth2 192.168.2.2 mask 255.255.255.0
+ifconfig eth3 192.168.3.2 mask 255.255.255.0
+ifconfig eth5 192.168.5.2 mask 255.255.255.0
+ifconfig eth6 192.168.6.2 mask 255.255.255.0
+ifconfig eth7 192.168.7.2 mask 255.255.255.0
+
+intr=$(cat /proc/interrupts | grep -m 1 eth0 | grep -oP '[0-9]+' | head -n 1)
+
+echo 000001 > /proc/irq/$intr/smp_affinity
+echo 000002 > /proc/irq/$[$intr+1]/smp_affinity
+echo 000004 > /proc/irq/$[$intr+2]/smp_affinity
+echo 000008 > /proc/irq/$[$intr+3]/smp_affinity
+echo 000010 > /proc/irq/$[$intr+4]/smp_affinity
+sysctl -w net.ipv4.tcp_congestion_control=reno
