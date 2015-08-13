3eb680e0d126d98b6924fac4eaa7a92476944ee9
Yilong Geng <ygeng@google.com>
2015-08-13 15:25:21 -0700

    make merging correct

--- linux-4.1/net/ipv4/tcp_offload.c
+++ linux-4.1/net/ipv4/tcp_offload.c
@@ -264,6 +264,7 @@ struct sk_buff **tcp_gro_receive(struct sk_buff **head, struct sk_buff *skb)
 	flush |= (__force int)(flags & (TCP_FLAG_URG | TCP_FLAG_PSH |
 					TCP_FLAG_RST | TCP_FLAG_SYN |
 					TCP_FLAG_FIN | TCP_FLAG_CWR | TCP_FLAG_ECE));
+	flush |= off < skb_headlen(skb);
 	NAPI_GRO_CB(skb)->flush |= (flush != 0);
 	return NULL;
 
@@ -279,6 +280,9 @@ found:
 	flush |= (__force int)(flags & (TCP_FLAG_RST | TCP_FLAG_SYN | TCP_FLAG_FIN | TCP_FLAG_URG));
 	if (flush)
 	printk(KERN_NOTICE "flush1 %u\n", flush);
+	flush |= off < skb_headlen(skb);
+	if (flush)
+	printk(KERN_NOTICE "flush2 offset %u headlen %u\n", off, skb_headlen(skb));
 	//flush |= (__force int)(th->ack_seq ^ th2->ack_seq);
 	//if (flush)
 	//printk(KERN_NOTICE "flush3 %u\n", flush);
@@ -345,31 +349,44 @@ found:
 
 		if (before(seq_next, seq2)) {
 			//printk(KERN_NOTICE "enqueue0\n");
-			NAPI_GRO_CB(skb)->out_of_order_queue = ofo_queue;
-			NAPI_GRO_CB(skb)->prev = NAPI_GRO_CB(p2)->prev;
-			NAPI_GRO_CB(skb)->next = p2;
-			ofo_queue->qlen += len;
-			ofo_queue->skb_num++;
-			if (NAPI_GRO_CB(p2)->prev == NULL) {
-				ofo_queue->next = skb;
-				NAPI_GRO_CB(p2)->prev = skb;
-
-				*head = skb;
-				skb->next = p_next;
+			if ((flags & TCP_FLAG_PSH) && (seq == in_seq)) {
+				p3 = NAPI_GRO_CB(p2)->prev;
+				if (p3 != NULL) {
+					*head = p2;
+					p2->next = p_next;
+					skb_gro_flush(ofo_queue, p3);
+				}
+				NAPI_GRO_CB(skb)->flush = 1;
+				return NULL;
 			} else {
-				NAPI_GRO_CB(NAPI_GRO_CB(p2)->prev)->next = skb;
-				NAPI_GRO_CB(p2)->prev = skb;
-			}
+				NAPI_GRO_CB(skb)->out_of_order_queue = ofo_queue;
+				NAPI_GRO_CB(skb)->prev = NAPI_GRO_CB(p2)->prev;
+				NAPI_GRO_CB(skb)->next = p2;
+				ofo_queue->qlen += len;
+				ofo_queue->skb_num++;
+				if (NAPI_GRO_CB(p2)->prev == NULL) {
+					ofo_queue->next = skb;
+					NAPI_GRO_CB(p2)->prev = skb;
 
-			NAPI_GRO_CB(skb)->same_flow = 1;
-			break;
+					*head = skb;
+					skb->next = p_next;
+				} else {
+					NAPI_GRO_CB(NAPI_GRO_CB(p2)->prev)->next = skb;
+					NAPI_GRO_CB(p2)->prev = skb;
+				}
+
+				NAPI_GRO_CB(skb)->same_flow = 1;
+				return NULL;
+			}
 
 		} else if (seq_next == seq2) {
 			//printk(KERN_NOTICE "enqueue1\n");
 
 			if ((err = skb_gro_merge(skb, p2))) {
-
-				if (err != SKB_MERGE_INVAL && in_seq != seq) {
+				if (err == SKB_MERGE_INVAL) {
+					NAPI_GRO_CB(skb)->flush = 1;
+					return head;
+				} else if (in_seq != seq) {
 					NAPI_GRO_CB(skb)->out_of_order_queue = ofo_queue;
 					NAPI_GRO_CB(skb)->prev = NAPI_GRO_CB(p2)->prev;
 					NAPI_GRO_CB(skb)->next = p2;
@@ -387,9 +404,9 @@ found:
 					}
 
 					NAPI_GRO_CB(skb)->same_flow = 1;
-					break;
+					return NULL;
 
-				} else if (err == SKB_MERGE_2BIG || err == SKB_MERGE_INVAL) {
+				} else if (err == SKB_MERGE_2BIG || tcp_hdr(p2)->psh || NAPI_GRO_CB(p2)->gso_end) {
 					printk(KERN_NOTICE "flush point 3\n");
 					p3 = NAPI_GRO_CB(p2)->next;
 					if (p3 != NULL) {
@@ -459,6 +476,11 @@ found:
 						p2 = p3;
 						continue;
 					} else {
+						if (in_seq == seq2 && th->psh) {
+							NAPI_GRO_CB(skb)->flush = 1;
+							return head;
+						}
+
 						NAPI_GRO_CB(skb)->out_of_order_queue = ofo_queue;
 						NAPI_GRO_CB(skb)->prev = p2;
 						NAPI_GRO_CB(skb)->next = NULL;
@@ -478,33 +500,30 @@ found:
 							skb_gro_flush(ofo_queue, p2);
 						}
 
-						break;
+						return NULL;
 					}
 
 				} else {
 					printk(KERN_NOTICE "flush point 4\n");
-					p3 = NAPI_GRO_CB(p2)->next;
-					if (p3 != NULL) {
-						*head = p3;
-						p3->next = p_next;
-						skb_gro_flush(ofo_queue, p2);
-						NAPI_GRO_CB(skb)->flush = 1;
-						return NULL;
-					} else {
-						NAPI_GRO_CB(skb)->flush = 1;
-						return head;
-					}
+					NAPI_GRO_CB(skb)->flush = 1;
+					return head;
 				}
 			}
 
-			th2 = tcp_hdr(p2);
-			tcp_flag_word(th2) |= flags & (TCP_FLAG_FIN | TCP_FLAG_PSH);
-
-			//skb_gro_free(skb);
-
 			ofo_queue->qlen += len;
-
+			th2 = tcp_hdr(p2);
 			p3 = NAPI_GRO_CB(p2)->next;
+			if (in_seq == seq2 && th2->psh) {
+				if (p3 == NULL) {
+					return head;
+				} else {
+					*head = p3;
+					p3->next = p_next;
+					skb_gro_flush(ofo_queue, p2);
+					return NULL;
+				}
+			}
+
 			if (p3 != NULL) {
 
 				if (seq_next == NAPI_GRO_CB(p3)->seq) {
@@ -522,15 +541,7 @@ found:
 						} else {
 							//printk(KERN_NOTICE "merge fail\n");
 							printk(KERN_NOTICE "flush point 5\n");
-							p4 = NAPI_GRO_CB(p3)->next;
-							if (p4 != NULL) {
-								*head = p4;
-								p4->next = p_next;
-								skb_gro_flush(ofo_queue, p3);
-								return NULL;
-							} else {
-								return head;
-							}
+							return head;
 						}
 					}
 
@@ -548,6 +559,18 @@ found:
 						NAPI_GRO_CB(NAPI_GRO_CB(p2)->next)->prev = p2;
 					}
 
+					p3 = NAPI_GRO_CB(p2)->next;
+					if (in_seq == seq2 && th2->psh) {
+						if (p3 == NULL) {
+							return head;
+						} else {
+							*head = p3;
+							p3->next = p_next;
+							skb_gro_flush(ofo_queue, p2);
+							return NULL;
+						}
+					}
+
 				} else if (after(seq_next, NAPI_GRO_CB(p3)->seq)) {
 					printk(KERN_NOTICE "flush point 7\n");
 					return head;
@@ -555,7 +578,7 @@ found:
 
 			}
 
-			break;
+			return NULL;
 
 		} else if (after(seq, seq_next2)) {
 			//printk(KERN_NOTICE "enqueue3\n");
